dbd4d5715ed9c178a14b17c6f5c9ea320c5a5eca
Ross Edlin <rossedlin@gmail.com>
2023-06-16 15:10:52 +0100

    Cleanuo old files

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker compose down
-docker build -t rossedlin/laravel:8.1 .
--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker compose run web composer create-project laravel/laravel laravel
